e9b552b5c3fa649ad84a84c8bfbe0b90e6e4c72b
vitaly <vitaly.koulakov@gmail.com>
2016-02-16 00:03:26 +0300

    Initial commit

---
+++ webui/src/main/java/com/wivern/WebuiApplication.java
@@ -0,0 +1,14 @@
+package com.wivern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class WebuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebuiApplication.class, args);
+	}
+}
